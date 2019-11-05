244da257371c247501aa8ba6137022cbf2aa2f6e
exsquire <34141973+exsquire@users.noreply.github.com>
2019-11-04 18:34:39 -0800

    Update DO_PIPE_scryptic.sh

--- custom/DO_PIPE_scryptic.sh
+++ custom/DO_PIPE_scryptic.sh
@@ -81,6 +81,7 @@ start <- ctrl[arrayid, 2]
 stop  <- ctrl[arrayid, 3]
 
 #Run scan1perm for autosomes only
+is_x_chr <- attr(apr, "is_x_chr")
 perm <- scan1perm(apr[,!is_x_chr],
                   $(echo 'cross$pheno[, start:stop, drop = FALSE]'),
                   kinship = kLOCO,
